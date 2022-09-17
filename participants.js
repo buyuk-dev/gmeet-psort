b0387121e290409b521422732ddca54a5254b351
Michal Michalski <michal@buyuk-dev.com>
2022-09-18 01:00:55 +0200

    Refactor; Replace button with periodic interval;
    Functions executed within the tab context can only use other functions available in that context.

--- participants.js
+++ participants.js
@@ -1,53 +1,81 @@
- function getParticipants() {
-    let participants = [];
-    let plist = $('div[jsname=jrQDbd]');
-    console.log(plist);
-    plist.children('div[role=listitem]').each(
-    (index, element) => {
-        let name = $(element).find("span.zWGUib").first().text();
-        let participant = {
-            item: element,
-            name: name,
+function updateParticipantsList() {
+    const listSelector = "div[jsname=jrQDbd]";
+    const itemSelector = "div[role=listitem]";
+    const nameSelector = "span.zWGUib";
+
+    function createParticipantFromHtml(html) {
+        return {
+            html: html,
+            name: $(html).find(nameSelector).first().text(),
             checked: false
         };
-        participants.push(participant);
-    });
+    }
 
-    participants.sort((a, b) => { return a.name < b.name ? 1 : -1; });
-    chrome.storage.sync.set({participants: participants}, () => {
-        console.log("setting participants list");
+    let current = $(listSelector).children(itemSelector).toArray().map(
+        (item) => { return createParticipantFromHtml(item); }
+    );
+
+    chrome.storage.sync.get("participants", ({participants}) => {
+        let updated = {};
+        for (const [name, participant] of Object.entries(current)) {
+            // if participant was present before, just copy it to preserve state
+            if (Object.hasOwn(participants, name)) {
+                updated[name] = participants[name];
+            }
+            // otherwise insert new participant.
+            else {
+                updated[name] = participant;
+            }
+        }
+        chrome.storage.sync.set({participants: updated}, () => {
+            console.log("participants list has been updated");
+        });
     });
 }
 
 
 function arrayToHtmlList(array) {
-    return array.map((e) => {
-        let li = $("<li>");
-        li.append(e);
-        return li;
+    return array.map((item) => {
+        return $("<li>").append(item);
     });
 }
 
-$(document).ready( () => {
 
-    let refreshAttendanceInterval = setInterval(() => {
-        chrome.storage.sync.get("participants", ({participants}) => {
-            let names = participants.map((p) => { return p.name; });
-            console.log("refreshing participants list");
-            console.log(names);
-            $("#attendance").empty();
-            $(arrayToHtmlList(names)).each((index, element) => {
-                $("#attendance").append($(element));
-            });
-        });
-    }, 1000);
+function updateParticipantsView() {
+    chrome.storage.sync.get("participants", ({participants}) => {
+        let compare = (a, b) => { return a.name < b.name ? 1 : -1; };
+        let names = arrayToHtmlList(Object.values(participants).sort(compare).map(
+            (item) => { return item.name; }
+        ));
 
-    $('#get-participants').click(async () => {
+        let list = $("#attendance");
+        list.empty();
+        $(names).each(
+            (_, item) => { list.append($(item)); }
+        );
+    });
+}
+
+
+function update() {
+    let updateModel = async () => {
         let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
         chrome.scripting.executeScript({
             target: {tabId: tab.id},
-            func: getParticipants
+            func: updateParticipantsList
         });
-    });
+    }
 
+    let updateView = () => {
+        updateParticipantsView();
+    }
+
+    updateModel();
+    updateView();
+}
+
+
+$(document).ready( () => {
+    const updateInterval = setInterval(update, 1000);
+    $('#get-participants').click(() => {});
 } );
